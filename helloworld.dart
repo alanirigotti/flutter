25ca0705568e4fa4c73e69d093fe706e57bd1a05
Alani Rigotti de Oliveira <103377188+alanirigotti@users.noreply.github.com>
2023-03-13 17:28:14 -0300

    meu primeiro hello world em dart

---
+++ helloworld.dart
@@ -0,0 +1,21 @@
+void main() {
+  print("Hello World!");
+
+  var name = "Alani";
+  var age = 20;
+  print("My name is ${name} and I am ${age}");
+
+  //this language looks like java!
+  /**
+   * Also de comments area the same!
+   */
+
+  // A List is an ordered group of objects.
+  var list = [1, 2, 3];
+
+  var heart = "\u2665";
+// it's funny that dart accepts single quotation marks
+  var emoji_laugh = '\u{1f600}';
+
+  print(heart);
+}
